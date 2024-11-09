d92bc0b2def7c2ee9b68bda617209086ee9cdc41
HashimotoShuhei-z <hassii0510@outlook.jp>
2024-11-10 01:21:13 +0900

    📦 NEW: 認証機能の実装

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('users/register', [AuthController::class, 'register']);
+Route::post('/admin/register', [AuthController::class, 'adminRegister']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
